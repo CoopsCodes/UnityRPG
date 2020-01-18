8232e3819f19783739c5e790f8b19e70b003e975
Cooper Viktor <im.cooperviktor@gmail.com>
2020-01-18 15:12:09 +1100

    animation and movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D theRB;
     public float moveSpeed;
 
+    public Animator myAnimator;
+
     // Start is called before the first frame update
     void Start() {
         
@@ -15,5 +17,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update() {
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        myAnimator.SetFloat("moveX", theRB.velocity.x);
+        myAnimator.SetFloat("moveY", theRB.velocity.y);
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            myAnimator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnimator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        } 
     }
 }
