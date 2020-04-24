dc62e01e59aa52bc7996591e4abeff4ee21822b0
Cooper Viktor <im.cooperviktor@gmail.com>
2020-04-24 12:34:22 +1000

    item script

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    // defining the types of items
+    public bool isItem;
+    public bool isWeapon;
+    public bool isArmour;
+
+    public string itemName;
+    public string itemDescription;
+    public int itemValue;
+    public Sprite itemSprite;
+
+    public int amountToChange; // the value of what the item impacts, ie health increase, weapon boost etc
+    public bool affectHP, affectMP, affectSRT;
+
+    public int weaponStrength;
+    public int armourStrength;
+
     // Start is called before the first frame update
     void Start()
     {
