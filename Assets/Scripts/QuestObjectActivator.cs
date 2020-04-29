767eaad23b6c6a4e8c4c8c60d4f6ea61cacb08ec
Cooper Viktor <im.cooperviktor@gmail.com>
2020-04-29 13:35:45 +1000

    quest avtivation marker logic completed

---
+++ Assets/Scripts/QuestObjectActivator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObjectActivator : MonoBehaviour
+{
+    public GameObject objectToActivate;
+
+    public string questToCheck;
+
+    public bool activeIfComplete;
+
+    private bool initialCheckDone; // checking to see if quest manager has been run in the scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!initialCheckDone) // checked after start once all objects have been loaded into the scene
+        {
+            initialCheckDone = true;
+
+            CheckCompletion();
+        }
+    }
+
+    public void CheckCompletion()
+    {
+        if (QuestManager.instance.CheckIfComplete(questToCheck))
+        {
+            objectToActivate.SetActive(activeIfComplete);
+        }
+    }
+}
