4a3db8ee21a3829143c9047a45c48494341af9f1
Cooper Viktor <im.cooperviktor@gmail.com>
2020-05-25 21:13:44 +1000

    refactoring

--- Assets/Scripts/LoadingScene.cs
+++ Assets/Scripts/LoadingScene.cs
@@ -6,11 +6,6 @@ using UnityEngine.SceneManagement;
 public class LoadingScene : MonoBehaviour
 {
     public float waitToLoad;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
